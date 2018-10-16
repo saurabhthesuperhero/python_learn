e7e08b0c5ee75ae20a04c6b2508c26f6a50d469a
young <657851574@qq.com>
2018-10-16 09:26:36 +0800

    pandas dataframe

--- stock/dataframe.py
+++ stock/dataframe.py
@@ -1,5 +1,25 @@
 #coding:utf-8
 
 import tushare as ts
+import pandas as pd
+import matplotlib.pyplot as plt
 
-ts.get_hist_data('600848')
+df = ts.get_hist_data('sz50')
+
+df.to_csv('./sz50.csv')#保存为csv文件
+
+print(df[0:10])#序号
+
+print(df['close'].max())#最大值
+
+print(df['open'].mean())#平均值
+
+#创建空的dataframe并以date_range中的时间序列为索引
+startDate = '2018-01-01'
+endDate = '2018-12-31'
+dates = pd.date_range(startDate,endDate)
+df1 = pd.DataFrame(index=dates)
+
+#绘图
+#df1[['low','high']].plot()
+#plt.show()
